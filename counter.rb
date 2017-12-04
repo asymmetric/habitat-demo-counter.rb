570a49802316e190c5541ab904cef473a72b39d2
Lorenzo Manacorda <lorenzo@kinvolk.io>
2017-12-04 23:48:04 +0100

    Remove CSS, update HAML
    Signed-off-by: Lorenzo Manacorda <lorenzo@kinvolk.io>

--- counter.rb
+++ counter.rb
@@ -22,70 +22,23 @@ __END__
 !!!
 %html
   %head
-    %meta{charset: 'utf-8'}
-    %meta{"http-equiv" => "content-type", content: "text/html; charset=UTF-8"}
-    %title
-      Habitat operator demo
-    :css
-      @import url('https://fonts.googleapis.com/css?family=Montserrat+Alternates|Titillium+Web');
-
-      h1 {
-          font-family: 'Montserrat Alternates', sans-serif;
-          font-wieght: 400;
-        font-size: 36px;
-      }
-      body {
-          font-family: 'Titillium Web', sans-serif;
-        background-color: rgb(40,60,76);
-        color: rgb(255,255,255);
-        text-align:center;
-        font-size: 16px;
-      }
-
-      .container {
-        width: 500px;
-        margin-top: 60px;
-        margin-right: auto;
-        margin-left: auto;
-      }
-
-      .counter {
-         width: 400px;
-          margin-right: auto;
-        margin-left: auto;
-      }
-
-      h3 {
-        font-weight: 100;
-        font-size: 25pt;
-        margin: 2pt;
-      }
-
-      p {
-        margin: 0;
-        font-size: 70pt;
-      }
-
-      input {
-        border: 2px;
-        border-style: solid;
-        padding: 10px;
-        color: rgb(255,255,255);
-        border-radius: 3px;
-        width: 120px;
-        background-color: rgba(0, 0, 0, 0);
-        border-color: rgb(255, 144, 18);
-        
-        font-family: 'Titillium Web', sans-serif;
-        font-size: 18pt;
-      }
+    %meta{:charset => "utf-8"}/
+    %title Habitat operator demo
+    %link{:href => "css/style.css", :rel => "stylesheet", :type => "text/css"}/
+    %link{:href => "images/favicon.ico", :rel => "shortcut icon", :type => "image/x-icon"}/
   %body
     .container
-      %h1
-        Hello from Habitat!
+      %h1 Welcome to the Habitat operator demo app.
       .counter
-        %h3
-          View count:
-          %span= @@counter
-        %form{action: "/reset", method: "post"}
-          %input{type: "submit", value: "reset"}
+        %h3 View count
+        %p.count= @@counter
+        %form{:action => "/reset", :method => "post"}
+          %input.button{:type => "submit", :value => "Reset"}/
+  %section.logos
+    .habitat-logo
+      %img{:alt => "Habitat logo", :src => "images/habitat-logo-by-chef.svg"}/
+    .kubernetes-logo
+      %img{:alt => "Kubernetes logo", :src => "images/kubernetes-logo.svg"}/
+  %footer
+    %img.habicat-jumping{:alt => "Habicat jumping", :src => "images/image-cat-jumping.svg"}/
+    %img.habicat-set{:alt => "Habicat standing with package", :src => "images/image-cat-set.svg"}/
